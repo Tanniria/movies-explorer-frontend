b71d7ccf5e11ffc75112ef812159de264aaa19d5
Tatiana Smakova <tensmakova@gmail.com>
2023-09-12 00:02:27 +0500

    2 итерация

--- src/utils/apiUtils.js
+++ src/utils/apiUtils.js
@@ -1,5 +1,5 @@
-export const BASE_URL = 'http://localhost:3000';
-// export const BASE_URL = 'https://api.movies-explorers.nomoredomains.xyz';
+// export const BASE_URL = 'http://localhost:3000';
+export const BASE_URL = 'https://api.movies-explorers.nomoredomains.xyz';
 export const MOVIES_URL = 'https://api.nomoreparties.co/beatfilm-movies';
 
 export const responseConstructor = async (url, method, body, isAuth) => {
